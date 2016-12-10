30479c4cd2e5202fad3bb254ea2eb54724234274
Felipe Jaramillo Gómez <pipeluck@gmail.com>
2016-12-09 22:33:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());                                                                                                                
+app.use(bodyParser.urlencoded({extended: true}));      
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var messages = [];
+var calls = 0;
+
+app.get('/message', function(req, res){
+  res.send(messages);
+  calls++;
+   console.log(calls);
+});
+
+app.post('/message', function(req, res){
+ messages.push(req.body);
+});
+
+io.on('connection', function(socket){
+     socket.broadcast.emit('hi');
+ 
+var messagesSocket = [];
+
+  socket.on('chat message', function(msg){
+      messagesSocket.push(msg);
+      io.emit('chat message', msg);
+            calls++;
+      console.log(calls);
+  });
+
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
